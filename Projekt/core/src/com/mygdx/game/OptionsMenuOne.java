f3b307bd9ff14e196def32cd9cbefe69b1cd86ac
SchnitzelLord <skido.geil@gmail.com>
2024-05-14 13:30:49 +0200

    added useless buttons

--- Projekt/core/src/com/mygdx/game/OptionsMenuOne.java
+++ Projekt/core/src/com/mygdx/game/OptionsMenuOne.java
@@ -7,6 +7,7 @@ import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.scenes.scene2d.Actor;
 import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.ImageButton;
 import com.badlogic.gdx.scenes.scene2d.ui.Skin;
 import com.badlogic.gdx.scenes.scene2d.ui.Table;
 import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
@@ -23,6 +24,8 @@ public class OptionsMenuOne implements Screen {
 
     Skin skin;
     Texture buttonBackground;
+    Texture minusButtonTexture;
+    Texture plusButtonTexture;
 
     public OptionsMenuOne(final Start game) {
         this.game = game;
@@ -31,8 +34,12 @@ public class OptionsMenuOne implements Screen {
         Gdx.input.setInputProcessor(stage);
 
         buttonBackground = new Texture(Gdx.files.internal("buttons\\buttonLayout.png"));
+        minusButtonTexture = new Texture(Gdx.files.internal("buttons\\minusButton.png"));
+        plusButtonTexture = new Texture(Gdx.files.internal("buttons\\plusButton.png"));
         skin = new Skin();
         skin.add("button", buttonBackground);
+        skin.add("minus", minusButtonTexture);
+        skin.add("plus", plusButtonTexture);
     }
 
     @Override
@@ -44,16 +51,16 @@ public class OptionsMenuOne implements Screen {
 
         TextButton.TextButtonStyle style = new TextButton.TextButtonStyle(skin.getDrawable("button"), skin.getDrawable("button"), skin.getDrawable("button"), new BitmapFont());
         TextButton backButton = new TextButton("Back", style);
+        ImageButton.ImageButtonStyle minusStyle = new ImageButton.ImageButtonStyle(skin.getDrawable("minus"), skin.getDrawable("minus"), skin.getDrawable("minus"), skin.getDrawable("minus"), skin.getDrawable("minus"), skin.getDrawable("minus"));
+        ImageButton.ImageButtonStyle plusStyle = new ImageButton.ImageButtonStyle(skin.getDrawable("plus"), skin.getDrawable("plus"), skin.getDrawable("plus"), skin.getDrawable("plus"), skin.getDrawable("plus"), skin.getDrawable("plus"));
+        ImageButton plusButton = new ImageButton(plusStyle);
+        ImageButton minusButton = new ImageButton(minusStyle);
 
 
         table.row().pad(20, 0, 20, 0);
-        /* table.add("Volume", "Aerial", Color.BLUE);
-
-        table.row();
-        table.add("Keybindings", "Aerial", Color.BLUE);
-
-        table.row();
-         */
+        table.add(minusButton);
+        table.add(plusButton);
+        table.row().pad(20, 0, 20, 0);
         table.add(backButton);
 
         backButton.addListener(new ChangeListener() {
