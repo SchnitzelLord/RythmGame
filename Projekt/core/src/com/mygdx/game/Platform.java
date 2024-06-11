d2d7b37e65536406c99dc8516c9fc2d06aa9cf6b
mphd123 <100949445+mphd123@users.noreply.github.com>
2024-06-11 12:41:07 +0200

    implemented all the textures with the rigth sprites, implemented a walkcycle and replaced the platforms with cars

--- Projekt/core/src/com/mygdx/game/Platform.java
+++ Projekt/core/src/com/mygdx/game/Platform.java
@@ -13,6 +13,7 @@ public class Platform extends Sprite {
 
     public enum Type {
         test,
+        car1,
     }
 
     public Platform(Type type, Texture texture, int srcX, int srcY, int srcWidth, int srcHeigth) {
@@ -30,18 +31,37 @@ public class Platform extends Sprite {
             if (player.getX() > this.getX() + this.getWidth()) return false;
             return true;
         }
+        else if (getType() == Type.car1) {
+            if (player.getX() + player.getWidth() < this.getX() + 180) return false;
+            if (player.getY() + player.getHeight() < this.getY() + 180) return false;
+            if (player.getY() > this.getY() + this.getHeight()) return false;
+            if (player.getX() > this.getX() + 380) return false;
+            return true;
+        }
         return false;
     }
 
     public static Platform createPlatform(Type type) {
-        return new Platform(type,getTexture(type), 0, 0, 100, 10);
-
+        Platform plat = new Platform(type, getTexture(type), 0, 0, 100, 10); // default shouldn't be the case
+        switch (type) {
+            case test:
+                plat = new Platform(type, getTexture(type), 0, 0, 100, 10);
+            break;
+            case car1:
+                plat =  new Platform(type, getTexture(type), 0, 0, 460, 200);
+            break;
+        }
+        return plat;
     }
+
+
 
     private static Texture getTexture(Type type) {
         Texture texture = null;
         switch (type) {
             case test: texture = new Texture("jumpAndRunSprites\\platform.png");
+                break;
+            case car1: texture = new Texture("jumpAndRunSprites\\car1.png");
                 break;
         }
         return  texture;
