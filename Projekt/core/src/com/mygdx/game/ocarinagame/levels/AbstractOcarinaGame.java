0e987b12be931c43f103f0ce11fb5006992baa70
mphd123 <100949445+mphd123@users.noreply.github.com>
2024-06-13 19:24:03 +0200

    Squashed commit of the following:
    commit 793d5e827c255c03a64c747e46d03ce184ebb081
    Author: hiya <145195079+hiroyaka@users.noreply.github.com>
    Date:   Thu Jun 13 18:18:50 2024 +0200
    
        Added delay after game is over to not immediately/abruptly switch screen.
    
    commit 31a6d8885810a21c6376c92b889e0a4724145383
    Author: hiya <145195079+hiroyaka@users.noreply.github.com>
    Date:   Thu Jun 13 17:06:47 2024 +0200
    
        Finished implementing Zelda Level without looping song. (Timing sometimes off, sometimes in sync)
    
        Added additional constructor for BeatMusic for automatic totalBeatCount calculation.
    
    commit b554dece44cb524c3ae48cb1d7fe611215b0eaa2
    Author: hiya <145195079+hiroyaka@users.noreply.github.com>
    Date:   Thu Jun 13 12:32:19 2024 +0200
    
        Updated win condition to be less strict.

---
+++ Projekt/core/src/com/mygdx/game/ocarinagame/levels/AbstractOcarinaGame.java
@@ -0,0 +1,268 @@
+package com.mygdx.game.ocarinagame.levels;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.math.MathUtils;
+import com.badlogic.gdx.scenes.scene2d.ui.Image;
+import com.badlogic.gdx.utils.Array;
+import com.badlogic.gdx.utils.Timer;
+import com.badlogic.gdx.utils.viewport.FillViewport;
+import com.badlogic.gdx.utils.viewport.Viewport;
+import com.mygdx.game.Conductor;
+import com.mygdx.game.MainMenuScreen;
+import com.mygdx.game.PauseScreen;
+import com.mygdx.game.Start;
+import com.mygdx.game.ocarinagame.Arrow;
+import com.mygdx.game.ocarinagame.BeatMusic;
+import com.mygdx.game.ocarinagame.ui.HUD;
+
+public abstract class AbstractOcarinaGame implements Screen {
+    // Game and screen sizes
+    protected static final int SCREEN_WIDTH = 1920;
+    protected static final int SCREEN_HEIGHT = 1080;
+    protected static final int WORLD_HEIGHT = 75;
+    protected static final int WORLD_WIDTH = WORLD_HEIGHT * 16 / 9;
+
+    // Offsets
+    protected static final float FLOAT_COMPARE_OFFSET = 0.01f; // for float comparisons (changing bpm at specific time)
+    protected static final float ARROW_SPAWN_POSITION_OFFSET = 3;
+
+    // Game state constants
+    protected static final float WIN_RATE = 0.5f;
+    protected static final float GAME_OVER_DELAY = 5;
+
+    // Game elements
+    protected final Start game;
+    protected final OrthographicCamera camera;
+    protected final Viewport viewport;
+
+    protected HUD hud;
+    protected Conductor conductor;
+    protected BeatMusic music;
+
+    // Game state
+    protected boolean isPenaltyOn;
+    protected boolean isRunning;
+    protected int score;
+    protected int lastArrowDirectionInt;
+
+    // Textures
+    protected final Texture arrowTexture;
+    protected Texture backgroundTexture;
+    protected Image background;
+
+    // Collections
+    protected final Array<Arrow> allArrows;
+
+    // Constructor
+
+    protected AbstractOcarinaGame(Start game) {
+        this.game = game;
+
+        // Setup camera
+        camera = new OrthographicCamera();
+        camera.setToOrtho(false, SCREEN_WIDTH, SCREEN_HEIGHT);
+
+        // Setup viewport for scaling
+        viewport = new FillViewport(WORLD_WIDTH, WORLD_HEIGHT, camera);
+        viewport.apply(true);
+
+        // Setup textures
+        arrowTexture = new Texture("ocarina-game\\arrow-up.png");
+
+        allArrows = new Array<>();
+
+        // Default values for game state
+        isPenaltyOn = false;
+        isRunning = false;
+        score = 0;
+        lastArrowDirectionInt = -1;
+    }
+
+    // Getter
+
+    public int getWorldWidth() {
+        return WORLD_WIDTH;
+    }
+
+    public int getWorldHeight() {
+        return WORLD_HEIGHT;
+    }
+
+    public int getScreenWidth() {
+        return SCREEN_WIDTH;
+    }
+
+    public int getScreenHeight() {
+        return SCREEN_HEIGHT;
+    }
+
+    public int getScore() {
+        return score;
+    }
+
+    // Overrides of functionality methods
+
+    @Override
+    public void render(float delta) {
+        if (isRunning) {
+            Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+            camera.update();
+            hud.update();
+        }
+    }
+
+    @Override
+    public void show() {
+        isRunning = true;
+        music.setVolume(Start.volume);
+    }
+
+    @Override
+    public void resize(int width, int height) {
+        viewport.update(width, height);
+    }
+
+    @Override
+    public void pause() {
+        switchToScreen(new PauseScreen(game, this));
+    }
+
+    @Override
+    public void resume() {
+        isRunning = true;
+        music.play();
+    }
+
+    @Override
+    public void hide() {
+        isRunning = false;
+        music.pause();
+    }
+
+    @Override
+    public void dispose() {
+        isRunning = false;
+        music.dispose();
+        arrowTexture.dispose();
+        backgroundTexture.dispose();
+        hud.dispose();
+    }
+
+    // Abstract methods
+
+    protected abstract void setupArrowSpawnPosition(Arrow arrow);
+
+    protected abstract void setBPM(int bpm);
+
+    protected abstract void controlsAction();
+
+    protected abstract boolean canArrowSpawn();
+
+    // Functionality & utility methods for subclasses
+
+    protected void reduceScore() {
+        // Reduce score only if player has any score points, no negative score
+        if (score > 0) score--;
+    }
+
+    protected void spawnArrow() {
+        Sprite sprite = new Sprite(arrowTexture, arrowTexture.getWidth(), arrowTexture.getHeight());
+        // Choose random direction for arrow
+        int dirInt = MathUtils.random(0, 3);
+
+        // Prevent arrow of same direction to spawn consecutively
+        while (lastArrowDirectionInt == dirInt) dirInt = MathUtils.random(0, 3);
+
+        sprite.setRotation(dirInt * 90); // counter-clock rotation, beginning with up-arrow
+        Arrow.Direction direction = Arrow.Direction.fromInt(dirInt);
+
+        Arrow a = new Arrow(sprite, direction, music.getPosition());
+        setupArrowSpawnPosition(a);
+        allArrows.add(a);
+
+        lastArrowDirectionInt = dirInt;
+    }
+
+    protected boolean isInputEqualsDirection(Arrow.Direction direction) {
+        // Check if correct key has been pressed
+        return isUpKeyPressed() && direction == Arrow.Direction.UP ||
+                isLeftKeyPressed() && direction == Arrow.Direction.LEFT ||
+                isDownKeyPressed() && direction == Arrow.Direction.DOWN ||
+                isRightKeyPressed() && direction == Arrow.Direction.RIGHT;
+    }
+
+    protected boolean isMissPressPenaltyTriggered() {
+        // Penalty for just pressing keys at random
+        return isPenaltyOn && (isUpKeyPressed() || isLeftKeyPressed() || isDownKeyPressed() || isRightKeyPressed());
+    }
+
+    protected void pauseGameOnEscape() {
+        // Check pause key being pressed
+        if (Gdx.input.isKeyJustPressed(Input.Keys.ESCAPE)) {
+            switchToScreen(new PauseScreen(game, this));
+        }
+    }
+
+    protected boolean isUpKeyPressed() {
+        return Gdx.input.isKeyJustPressed(Input.Keys.W) || Gdx.input.isKeyJustPressed(Input.Keys.UP);
+    }
+
+    protected boolean isLeftKeyPressed() {
+        return Gdx.input.isKeyJustPressed(Input.Keys.A) || Gdx.input.isKeyJustPressed(Input.Keys.LEFT);
+    }
+
+    protected boolean isRightKeyPressed() {
+        return Gdx.input.isKeyJustPressed(Input.Keys.D) || Gdx.input.isKeyJustPressed(Input.Keys.RIGHT);
+    }
+
+    protected boolean isDownKeyPressed() {
+        return Gdx.input.isKeyJustPressed(Input.Keys.S) || Gdx.input.isKeyJustPressed(Input.Keys.DOWN);
+    }
+
+    // For differentiating full score win with "acceptable" win
+    protected boolean isGamePerfectlyWon() {
+        return score >= music.getTotalBeatCount();
+    }
+
+    protected boolean hasWinRateBeenReached() {
+        return score >= music.getTotalBeatCount() * WIN_RATE;
+    }
+
+    protected void switchToScreen(Screen screen) {
+        isRunning = false;
+        music.pause();
+        game.setScreen(screen);
+    }
+
+    protected void drawArrows() {
+        // Setup SpriteBatch and draw arrows
+        game.batch.setProjectionMatrix(camera.combined);
+        game.batch.begin();
+
+        for (Arrow currArrow : allArrows) {
+            currArrow.getSprite().draw(game.batch);
+        }
+
+        game.batch.end();
+    }
+
+    // Check whether game has been won or not after song is over + GAME_OVER_DELAY
+    // and switch screen accordingly
+    protected void delayedGameOverWinCheck(float songLength) {
+        new Timer().scheduleTask(new Timer.Task() {
+            @Override
+            public void run() {
+                if (hasWinRateBeenReached())
+                    switchToScreen(new MainMenuScreen(game));
+                else {}
+                dispose();
+            }
+        }, songLength + GAME_OVER_DELAY);
+    }
+}
---
+++ Projekt/core/src/com/mygdx/game/ocarinagame/levels/OcarinaGameFalling.java
@@ -0,0 +1,202 @@
+package com.mygdx.game.ocarinagame.levels;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.audio.Music;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.scenes.scene2d.ui.Image;
+import com.badlogic.gdx.utils.Array;
+import com.mygdx.game.Conductor;
+import com.mygdx.game.Start;
+import com.mygdx.game.ocarinagame.Arrow;
+import com.mygdx.game.ocarinagame.BeatMusic;
+import com.mygdx.game.ocarinagame.ui.HUD;
+import com.mygdx.game.ocarinagame.ui.ScoreProgressBar;
+
+import java.util.Iterator;
+
+public final class OcarinaGameFalling extends AbstractOcarinaGame {
+
+    // Game state constants
+    private static final float SPEED = 75;
+    private static final float DELAY = 3;
+
+    // Arrow hit and spawn position
+    private static final float HIT_POSITION = 1;
+
+    // Hitzone
+    private final Texture hitZoneTexture;
+    private final Sprite hitZone;
+
+    // Constructor
+
+    public OcarinaGameFalling(Start game) {
+        // Setup common game elements like
+        // camera, viewpoint, textures, allArrows
+        super(game);
+
+        // Setup audio
+        Music song = Gdx.audio.newMusic(Gdx.files.internal("Music\\zelda_fight_150bpm.mp3"));
+        music = new BeatMusic(song, 150, 0, 37.144f, 37.878f);
+        music.playMusicAfterSec(DELAY);
+        song.dispose();
+        conductor = new Conductor(music.getBPM(), 0);
+        conductor.start();
+        // Set first beat time stamp with delay since beat begins immediately
+        conductor.lastBeat = music.getBeatStart() - conductor.crochet + DELAY;
+
+        // Timer to switch to another screen depending on result after GAME_OVER_DELAY
+        delayedGameOverWinCheck(music.getSongLength());
+
+        // Setup UI
+        // Progressbar max is set to WIN_RATE * totalBeatCount, e.g. is progress bar full then the game is won
+        hud = new HUD(game.batch, this);
+        ScoreProgressBar progressBar = hud.getProgressBar();
+        // Position at top right corner
+        progressBar.setPosition(WORLD_WIDTH - progressBar.getWidth() - 3, WORLD_HEIGHT - progressBar.getHeight() - 3);
+        progressBar.setRange(0, music.getTotalBeatCount() * WIN_RATE);
+
+        // Setup hitZone
+        hitZoneTexture = new Texture("ocarina-game\\hit-zone.png");
+        hitZone = new Sprite(hitZoneTexture, hitZoneTexture.getWidth(), hitZoneTexture.getHeight());
+        hitZone.setPosition(0.5f * (WORLD_WIDTH - hitZone.getWidth()), HIT_POSITION );
+
+        // Setup background
+        backgroundTexture = new Texture("ocarina-game\\zelda-background.png");
+        background = new Image(backgroundTexture);
+    }
+
+    // Overrides
+
+    @Override
+    public void render(float delta) {
+        // Clear screen, update HUD & camera
+        super.render(delta);
+
+        if (isRunning) {
+            // Check input and act upon them
+            controlsAction();
+
+            draw();
+
+            // Spawn, move and delete arrows
+            if (canArrowSpawn()) spawnArrow();
+            moveArrowsDown(delta);
+            deleteArrowsOutOfWorld();
+        }
+    }
+
+    @Override
+    public void dispose() {
+        super.dispose();
+        hitZoneTexture.dispose();
+    }
+
+    @Override
+    protected void setupArrowSpawnPosition(Arrow arrow) {
+        Arrow.Direction direction = arrow.getDirection();
+        Sprite sprite = arrow.getSprite();
+
+        // Position anchors at bottom left of sprite
+        switch (direction) {
+            case LEFT:
+                sprite.setPosition((WORLD_WIDTH - ARROW_SPAWN_POSITION_OFFSET) * 0.5f - sprite.getWidth() * 2 - ARROW_SPAWN_POSITION_OFFSET, WORLD_HEIGHT);
+                break;
+            case UP:
+                sprite.setPosition((WORLD_WIDTH - ARROW_SPAWN_POSITION_OFFSET) * 0.5f - sprite.getWidth(), WORLD_HEIGHT);
+                break;
+            case DOWN:
+                sprite.setPosition((WORLD_WIDTH + ARROW_SPAWN_POSITION_OFFSET) * 0.5f, WORLD_HEIGHT);
+                break;
+            case RIGHT:
+                sprite.setPosition((WORLD_WIDTH + ARROW_SPAWN_POSITION_OFFSET) * 0.5f + sprite.getWidth() + ARROW_SPAWN_POSITION_OFFSET, WORLD_HEIGHT);
+                break;
+        }
+    }
+
+    @Override
+    protected void setBPM(int bpm) {
+        conductor = new Conductor(bpm, 0);
+        conductor.lastBeat = music.getPosition() + conductor.crochet;
+        conductor.start();
+        music.setBPM(bpm);
+    }
+
+    @Override
+    protected void controlsAction() {
+        pauseGameOnEscape();
+
+        // Look for any arrows that have reached hitZone and remember them
+        Array<Arrow> arrowsInHitZone = new Array<>();
+        for (Arrow a : allArrows) {
+            if (isArrowInHitZone(a)) arrowsInHitZone.add(a);
+        }
+
+        // Check for every arrow that is in hitZone if corresponding key has been pressed
+        for (Arrow arrow : arrowsInHitZone) {
+            if (isArrowInHitZone(arrow) && isInputEqualsDirection(arrow.getDirection())) {
+                score++;
+                allArrows.removeValue(arrow, true);
+            } else if (isMissPressPenaltyTriggered()) {
+                reduceScore();
+            }
+        }
+    }
+
+    @Override
+    protected boolean canArrowSpawn() {
+        // Calculate distance from arrow to center of hitZone and time needed to reach it
+        // Coordinates anchor at bottom left
+        float hitZoneCenterPos = hitZone.getY() + (hitZone.getHeight() - arrowTexture.getHeight()) * 0.5f;
+        float distance = WORLD_HEIGHT - hitZoneCenterPos;
+        float travelTime = (distance / SPEED) * Gdx.graphics.getDeltaTime();
+
+        // Calculate time when next beat will happen
+        // Should happen _travelTime_ seconds earlier, so that arrow will reach HIT_POSITION when beat happens
+        conductor.nextBeatTime = conductor.lastBeat + conductor.crochet - travelTime;
+
+        // Spawn arrow when nextBeatTime is reached
+        if (music.getPosition() >= conductor.nextBeatTime && music.getPosition() <= music.getBeatEnd()) {
+            conductor.lastBeat = music.getPosition();
+            return true;
+        }
+        return false;
+    }
+
+    // Private function & utility methods
+
+    private boolean isArrowInHitZone(Arrow arrow) {
+        int offset = 2;
+        return hitZone.getY() - offset <= arrow.getSprite().getY() &&
+                arrow.getSprite().getY() <= hitZone.getY() + offset + hitZone.getHeight() - arrowTexture.getHeight();
+    }
+
+    private void moveArrowsDown(float delta) {
+        for (Arrow a : allArrows) {
+            a.getSprite().translateY(-SPEED * delta);
+        }
+    }
+
+    private void deleteArrowsOutOfWorld() {
+        for (Iterator<Arrow> it = allArrows.iterator(); it.hasNext(); ) {
+            Sprite s = it.next().getSprite();
+            if (s.getY() < -s.getHeight()) {
+                it.remove();
+                if (isPenaltyOn) reduceScore();
+            }
+        }
+    }
+
+    private void draw() {
+        // Start spriteBatch and draw hitZone
+        game.batch.setProjectionMatrix(camera.combined);
+        game.batch.begin();
+        background.draw(game.batch, 1.0f);
+        hitZone.draw(game.batch);
+        game.batch.end();
+
+        hud.draw();
+
+        drawArrows();
+    }
+}
