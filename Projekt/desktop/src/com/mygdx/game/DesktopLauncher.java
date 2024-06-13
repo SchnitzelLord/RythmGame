f3924990ba61e59b627884ca7b38078ac9577639
SchnitzelLord <168120859+SchnitzelLord@users.noreply.github.com>
2024-06-13 21:59:41 +0200

    added icon

--- Projekt/desktop/src/com/mygdx/game/DesktopLauncher.java
+++ Projekt/desktop/src/com/mygdx/game/DesktopLauncher.java
@@ -10,6 +10,7 @@ public class DesktopLauncher {
 		config.setForegroundFPS(60);
 		config.setTitle("Day By Day");
 		config.setFullscreenMode(Lwjgl3ApplicationConfiguration.getDisplayMode());
+		config.addIcon("Menus\\DayByDay.png", Files.FileType.Internal);
 		//config.setWindowedMode(1280, 720);
 		new Lwjgl3Application(new Start(), config);
 	}
